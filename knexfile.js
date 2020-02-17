64878ced19a4cd3db336ae24776b7ec254d51038
korstan <stas.korda@gmail.com>
2020-02-17 22:56:47 +0500

    test fix

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
